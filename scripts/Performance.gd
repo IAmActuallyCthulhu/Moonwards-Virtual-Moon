1d74b4ec59caf626b2081fc48aaa9ae8a8e2deab
MolbOrg <MolbOrg@gmail.com>
2018-11-15 02:20:51 +0300

    performance monitor changes

--- scripts/Performance.gd
+++ scripts/Performance.gd
@@ -4,16 +4,15 @@ export(NodePath) var source_xyz = "Camera"
 
 
 func _process(delta):
-	$FPS.text = str(Performance.get_monitor(Performance.TIME_FPS))
-	$Objects.text = str(Performance.get_monitor(Performance.RENDER_OBJECTS_IN_FRAME))
-	$Vertices.text = str(Performance.get_monitor(Performance.RENDER_VERTICES_IN_FRAME))
-	$VRAM.text = str(Performance.get_monitor(Performance.RENDER_VIDEO_MEM_USED))
+	$Panel/FPS.text = str(Performance.get_monitor(Performance.TIME_FPS))
+	$Panel/Objects.text = str(Performance.get_monitor(Performance.RENDER_OBJECTS_IN_FRAME))
+	$Panel/Vertices.text = str(Performance.get_monitor(Performance.RENDER_VERTICES_IN_FRAME))
+	$Panel/VRAM.text = str(Performance.get_monitor(Performance.RENDER_VIDEO_MEM_USED))
 	
 	var xyz = self.get_node(source_xyz)
 	
 	if xyz :
-		var ORIGIN = xyz.to_global(xyz.transform.origin)
-		#var ORIGIN = xyz.translation
-		$x.text = str(ORIGIN.x)
-		$y.text = str(ORIGIN.y)
-		$z.text = str(ORIGIN.z)
+		var ORIGIN = xyz.translation
+		$Panel/x.text = str(ORIGIN.x)
+		$Panel/y.text = str(ORIGIN.y)
+		$Panel/z.text = str(ORIGIN.z)
